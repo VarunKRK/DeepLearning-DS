35ef8c38fd726cb3a61c4da2e7aa9f9dd027a925
Varun Kota <vkr.k8055@gmail.com>
2022-04-06 17:34:28 +0200

    Update train.py

--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -14,7 +14,7 @@ model = Regression(8)
 
 x_train, x_test, y_train, y_test = dh.load_data(r'Chapter 03\03. MLP Regression\data\turkish_stocks.csv')
 
-epochs = 10
+epochs = 100
 
 
 criterion = nn.MSELoss()
@@ -26,7 +26,10 @@ MSE = []
 
 for epoch in range(epochs):
 
-    x_trbh, x_tsbh, y_trbh, y_tsbh = dh.to_batches(x_train, x_test, y_train, y_test, 5)
+    running_train_loss = 0
+    running_test_loss = 0
+
+    x_trbh, x_tsbh, y_trbh, y_tsbh = dh.to_batches(x_train, x_test, y_train, y_test, 20)
     for i in range(x_trbh.shape[0]):
         
 
@@ -47,11 +50,16 @@ for epoch in range(epochs):
 
         model.train()
 
-        loss_train.append(train_loss.item())
-        loss_test.append(test_loss.item())
+        running_train_loss += train_loss.item()
+        running_test_loss += test_loss.item()
+
+        #loss_train.append(train_loss.item())
+        #loss_test.append(test_loss.item())
        
         print(f'Epoch: {epoch + 1} | loss: {train_loss.item()} | test loss: {test_loss.item()}' )
 
+    loss_train.append(running_train_loss/len(x_trbh))
+    loss_test.append(running_test_loss/len(x_tsbh))
 
 plt.plot(loss_train, label='train Loss')
 plt.plot(loss_test, label='test Loss')
